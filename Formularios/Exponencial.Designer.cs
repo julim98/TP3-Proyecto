a66f6c5deffcd9690e8e2d8327a0e800d051863f
Julián Mansilla <julianm_98@hotmail.com>
2021-04-28 20:12:27 -0300

    exponencial sin probar

---
+++ Formularios/Exponencial.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace TP3_proyecto.Formularios
+{
+    partial class Exponencial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbParametro = new System.Windows.Forms.ComboBox();
+            this.txtParametro = new System.Windows.Forms.TextBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.grilla = new System.Windows.Forms.DataGridView();
+            this.numero = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.valor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnVolver = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grilla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Seleccion parametro";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(88, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Cantidad";
+            // 
+            // cmbParametro
+            // 
+            this.cmbParametro.FormattingEnabled = true;
+            this.cmbParametro.Location = new System.Drawing.Point(17, 110);
+            this.cmbParametro.Name = "cmbParametro";
+            this.cmbParametro.Size = new System.Drawing.Size(121, 21);
+            this.cmbParametro.TabIndex = 3;
+            // 
+            // txtParametro
+            // 
+            this.txtParametro.Location = new System.Drawing.Point(143, 110);
+            this.txtParametro.Name = "txtParametro";
+            this.txtParametro.Size = new System.Drawing.Size(100, 20);
+            this.txtParametro.TabIndex = 4;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(143, 161);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidad.TabIndex = 4;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(140, 207);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerar.TabIndex = 5;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(24, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(219, 24);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Dsitribucion Exponencial";
+            // 
+            // grilla
+            // 
+            this.grilla.AllowUserToAddRows = false;
+            this.grilla.AllowUserToDeleteRows = false;
+            this.grilla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grilla.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.numero,
+            this.valor});
+            this.grilla.Location = new System.Drawing.Point(361, 31);
+            this.grilla.Name = "grilla";
+            this.grilla.ReadOnly = true;
+            this.grilla.Size = new System.Drawing.Size(301, 199);
+            this.grilla.TabIndex = 6;
+            // 
+            // numero
+            // 
+            this.numero.HeaderText = "Número";
+            this.numero.Name = "numero";
+            this.numero.ReadOnly = true;
+            this.numero.Width = 80;
+            // 
+            // valor
+            // 
+            this.valor.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.valor.HeaderText = "Valor";
+            this.valor.Name = "valor";
+            this.valor.ReadOnly = true;
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Location = new System.Drawing.Point(42, 207);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(75, 23);
+            this.btnVolver.TabIndex = 7;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = true;
+            this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // Exponencial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 259);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.grilla);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.txtParametro);
+            this.Controls.Add(this.cmbParametro);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Name = "Exponencial";
+            this.Text = "Exponencial";
+            this.Load += new System.EventHandler(this.Exponencial_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grilla)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbParametro;
+        private System.Windows.Forms.TextBox txtParametro;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView grilla;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numero;
+        private System.Windows.Forms.DataGridViewTextBoxColumn valor;
+        private System.Windows.Forms.Button btnVolver;
+    }
+}
---
+++ Formularios/Exponencial.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP3_proyecto.Formularios
+{
+    public partial class Exponencial : Form
+    {
+        public Exponencial()
+        {
+            InitializeComponent();
+        }
+
+        enum Parametro
+        {
+            Lambda,
+            Media,
+            Varianza
+        }
+
+        private void Exponencial_Load(object sender, EventArgs e)
+        {
+            cmbParametro.Items.Add(Parametro.Lambda.ToString());
+            cmbParametro.Items.Add(Parametro.Media.ToString());
+            cmbParametro.Items.Add(Parametro.Varianza.ToString());
+        }
+
+        private bool verificarEntradas()
+        {
+            if (!int.TryParse(txtParametro.Text, out int resultado1) || resultado1 <= 0)
+            {
+                MessageBox.Show("Ingrese correctamente el parametro");
+                return false;
+            }
+
+            if (!int.TryParse(txtCantidad.Text, out int resultado2) || resultado2 <= 0)
+            {
+                MessageBox.Show("Ingrese correctamente la cantidad de variables aleatorias");
+                return false;
+            }
+            return true;
+        }
+
+        private decimal completar(int numero)
+        {
+            double lambda;
+            if (cmbParametro.Text == Parametro.Lambda.ToString())
+            {
+                lambda = numero;
+            }
+            else if (cmbParametro.Text == Parametro.Media.ToString())
+            {
+                lambda = 1 / numero;
+            }
+            else
+            {
+                lambda = 1 / Math.Sqrt(numero);
+            }
+            return decimal.Parse(lambda.ToString());
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            if (verificarEntradas())
+            {
+                var rand = new Random();
+                int cantidad = int.Parse(txtCantidad.Text);
+                decimal lambda = completar(int.Parse(txtParametro.Text));
+                grilla.Rows.Clear();
+                grilla.Rows.Add(cantidad);
+
+                for (int i = 0; i < cantidad ; i++)
+                {
+                    decimal x = decimal.Round(- (1 / lambda) * (1 - rand.Next(0, 1)), 4);
+                    grilla.Rows[i].Cells[0].Value = i++;
+                    grilla.Rows[i].Cells[0].Value = x;
+                }
+            }
+        }
+
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+}
+
